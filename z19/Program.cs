d50e8ba12bed773e4fd91b8fa8bb3e555eb097e9
Ershov Dmitrii <dima52052@mail.ru>
2022-09-01 14:24:12 +0500

    Add z19,z21,z23

---
+++ z19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+
+void ChekNumber(string number){
+    if (number[0]==number[4] || number[1]==number[3])  
+        {Console.WriteLine($"{number} - палиндром");}
+    else   
+        Console.WriteLine($"{number} - не палиндром");
+}
+
+ChekNumber(number);
---
+++ z23/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[] Cube(int n){
+    double[] arr=new double[n];
+
+    for (int i = 1; i <= n; i++)            
+    {
+        Console.WriteLine(Math.Pow(i, 3)); 
+    }
+
+    return arr;
+}
+
+Cube(n);
