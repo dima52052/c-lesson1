7bd1851e1e00ffed7f64b509d2a0fe17fbba6ad1
Ershov Dmitrii <dima52052@mail.ru>
2022-09-05 14:50:10 +0500

    Add z25,z27,z29

---
+++ z29/Program.cs
@@ -0,0 +1,12 @@
+﻿int [] GArr(int size=8){
+    int[] arr=new int[size];
+    for (int i=0; i<arr.Length;i++){
+        arr[i]=new Random().Next(100);
+    }
+
+    return arr;
+}
+
+foreach (var item in GArr()){
+Console.WriteLine(item);
+}
