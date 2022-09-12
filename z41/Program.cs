f67ea72a85b3465aa74d5c84686037af9099ae89
Ershov Dmitrii <dima52052@mail.ru>
2022-09-12 15:50:55 +0500

    Add z41,z43

---
+++ z41/Program.cs
@@ -0,0 +1,21 @@
+﻿int ex41()
+{
+Console.Write("Введите числа через Enter, для окончания ввода введите stop: ");
+int sum=0;
+for (int i=0; ; i++)
+{
+    string n= Console.ReadLine();
+    if (n == "stop") break;
+    else
+        {
+            int m=Convert.ToInt32(n);
+            if (m>0) sum++;
+        }
+}
+Console.Write(sum);
+return sum;
+}
+
+
+ex41();
+
---
+++ z43/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double[] Point(double b1,double k1,double b2,double k2) 
+{
+    double[] result=new double[2];
+    result[0]=(b2-b1)/(k1-k2);
+    result[1]=k1*result[0]+b1;
+
+    return result;
+}
+Console.WriteLine(string.Join(" ",Point(b1,k1,b2,k2)));
